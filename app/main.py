18a6ecc07fddde9225d4e653eb43fc43803289c8
n2oneProgrammer <dominikwojtasik191@gmail.com>
2021-01-27 16:39:31 +0100

    add db to gitignore

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
 from fastapi import FastAPI
 from .database import engine, Base
+
 Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello!"}
