0a4661fb5383332522146527a881c0dd8e9e36d1
n2oneProgrammer <dominikwojtasik191@gmail.com>
2021-02-01 23:06:21 +0100

    get enpoints

--- app/routers/users_router.py
+++ app/routers/users_router.py
@@ -1,12 +1,12 @@
 from typing import List
 
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, HTTPException
 from sqlalchemy.orm import Session
 
 from app import crud
-from app.dependencies import get_db
+from app.dependencies import get_db, get_current_user, have_user_permission
 from app.models.account_type import AccountType
-from app.schema import UserResponse
+from app.schema import UserResponse, User
 
 router = APIRouter(tags=["user"])
 
@@ -15,3 +15,36 @@ router = APIRouter(tags=["user"])
 async def get_organizations(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users_by_type(db, AccountType.ORGANIZATION, skip, limit)
     return users
+
+
+@router.get("/admin/users", response_model=List[UserResponse], responses={
+    401: {
+        "description": "Not authenticated, you must have Admin permission"
+    }
+})
+async def get_all_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db),
+                        current_user: User = Depends(get_current_user)):
+    have_user_permission(current_user, [AccountType.ADMIN])
+    users = crud.get_users(db, skip, limit)
+    return users
+
+
+@router.get("/users/me", response_model=UserResponse, responses={
+    401: {
+        "description": "Not authenticated, you must have Admin permission"
+    }
+})
+async def get_user_me(current_user: User = Depends(get_current_user)):
+    return current_user
+
+
+@router.get("/users/{user_id}", response_model=UserResponse, responses={
+    404: {
+        "description": "User not found"
+    }
+})
+async def get_user_by_user_id(user_id: int, db: Session = Depends(get_db)):
+    user = crud.get_user_by_userid(db, user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
